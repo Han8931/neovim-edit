e5fe15ca5b6bbe3c4238997ab8921efcf73ea5ff
Han <tabularasa8931@gmail.com>
2024-04-14 12:17:05 +0900

    Init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# NeoVim Integration for Obsidian
+
+This plugin allows you to seamlessly use NeoVim to edit your notes within Obsidian, combining the power of NeoVim's text editing capabilities with Obsidian's note-taking and knowledge management features.
+
+## Features
+
+- **Seamless Integration:** Open your current note in NeoVim with a simple command or shortcut (`Ctrl+Alt o`), making your editing workflow faster and more efficient.
+
+## Installation
+1. Clone this repo and put the entire directory under your `<vault>/plugins/`
+2. Toggle the plugin (community plugins in the settings) 
+
+## Usage
+
+To edit your current note in NeoVim, use the following steps:
+
+1. Open the note you wish to edit in Obsidian.
+2. Press `Ctrl+Alt o`. Your note will open in NeoVim. Make your edits as needed.
+3. Save your changes in NeoVim. The edited content will automatically update in Obsidian.
+
+## Configuration
+Change the `terminalCommand` in `main.js` to your default terminal. In my case, my default terminal is `simple terminal (st)`.
+
+## Contributing
+
+Contributions are welcome! If you'd like to improve the plugin, please fork the repository and submit a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+
---
+++ main.js
@@ -0,0 +1,66 @@
+const { Plugin } = require("obsidian");
+const { exec } = require("child_process");
+const os = require("os");
+const path = require("path");
+
+class TerminalPlugin extends Plugin {
+	async onload() {
+		this.addCommand({
+			id: "nvim-edit",
+			name: "neovim-edit",
+			hotkeys: [
+			  {
+				modifiers: ["Ctrl", "Alt"], // Use "Mod" instead of "Ctrl" if you want it to work as "Cmd" on macOS and "Ctrl" on Windows/Linux.
+				key: "o",
+			  },
+			],
+			callback: () => {
+				this.openInTerminal();
+			},
+		});
+	}
+
+	openInTerminal() {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (activeFile) {
+			// const filePath = activeFile.path;
+			const filePath = path.join(this.app.vault.adapter.basePath, activeFile.path);
+			let terminalCommand;
+			switch (os.platform()) {
+				case "darwin": // macOS
+					terminalCommand = `osascript -e 'tell app "Terminal" to do script "nvim ${filePath}"'`;
+					break;
+				case "win32": // Windows
+					// Adjust accordingly for PowerShell and Neovim
+					terminalCommand = `powershell -NoProfile -Command "Start-Process nvim -ArgumentList '${filePath}'"`;
+					break; 
+				case "linux":
+					// Replace 'gnome-terminal' with 'st' for Simple Terminal
+					terminalCommand = `st -e nvim '${filePath}'`;
+					break;
+
+				default:
+					console.error("Unsupported platform");
+					return;
+			}
+
+			exec(terminalCommand, (error, stdout, stderr) => {
+				if (error) {
+					console.error(`exec error: ${error}`);
+					return;
+				}
+				console.log(`stdout: ${stdout}`);
+				console.error(`stderr: ${stderr}`);
+			});
+		} else {
+			console.log("No active file found.");
+		}
+	}
+
+	onunload() {
+		// Clean up any event listeners or other resources
+	}
+}
+
+module.exports = TerminalPlugin;
+
